7f3d316d0d90cec97ed73d759db24771cee4d830
Mr-Dan-kibet <dan.rotich@student.moringaschool.com>
2025-06-04 23:38:18 +0300

    understanding statements

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numberOfFeet) {
   // Write your code here!
+  if (numberOfFeet <= 400) {
+    return `This one is on me!`;
+  } else if (numberOfFeet > 400 && numberOfFeet <= 2000) {
+    return `That will be twenty bucks.`;
+  } else if (numberOfFeet > 2000 && numberOfFeet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (numberOfFeet > 2500) {
+    return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === `NYC` ? `Ok, sounds good.` : `No go.`;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case `generous`:
+      return "Thank you so much.";
+    case `not as generous`:
+      return "Thank you.";
+    default:
+      return `Bye.`;
+  }
 }
